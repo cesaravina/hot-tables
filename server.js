75386f38f8d26d16470616680946abb4a23703db
cesar avina <cesaravina@gmail.com>
2017-04-22 13:17:04 -0700

    workig code

--- server.js
+++ server.js
@@ -14,21 +14,22 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Hot Tables
-var hotTables = [{
-	routeName: 'table1',
-	name: 'Person 1',
-	phone: "Phone Number",
-	email: "Email",
-	id: "Unique ID",
-}];
+var hotTables = [
+// 	name: 'Person 1',
+// 	phone: "Phone Number",
+// 	email: "Email",
+// 	id: "Unique ID",
+// }
+];
 
-var waitTables = [{
-	routeName: 'table2',
-	name: 'Person 2',
-	phone: "Phone Number",
-	email: "Email",
-	id: "Unique ID",
-}]
+var waitTables = [
+// {
+// 	name: 'Person 2',
+// 	phone: "Phone Number",
+// 	email: "Email",
+// 	id: "Unique ID",
+// }
+]
 
 // Routes
 app.get("/", function(req, res){
@@ -53,16 +54,21 @@ app.get("/api/get-waitlist", function(req, res){
 
 app.post("/api/make-reservation", function(req, res){
 	var newTable = req.body;
-	newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+	// newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
 	console.log(newTable);
+
+
+
+	if(hotTables.length<=4){
+		hotTables.push(newTable);
+		newTable.wait = true;
+	}else{
+		waitTables.push(newTable)
+		newTable.wait = false;
+	}
 
 	res.json(newTable);
 
-	if(hotTables.length<=5){
-		hotTables.push(newTable);
-	}else{
-		waitTables.push(newTable)
-	}
 });
 
 // Listener
