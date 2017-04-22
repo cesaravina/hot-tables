5db8de503e06a87f05e453950d6aad8df98f0fab
cesar avina <cesaravina@gmail.com>
2017-04-22 12:38:18 -0700

    creating reservations

--- server.js
+++ server.js
@@ -22,6 +22,14 @@ var hotTables = [{
 	id: "Unique ID",
 }];
 
+var waitTables = [{
+	routeName: 'table2',
+	name: 'Person 2',
+	phone: "Phone Number",
+	email: "Email",
+	id: "Unique ID",
+}]
+
 // Routes
 app.get("/", function(req, res){
 	res.sendFile(path.join(__dirname, "index.html"));
@@ -35,6 +43,27 @@ app.get("/reservations", function(req, res){
 	res.sendFile(path.join(__dirname, "reservations.html"));
 });
 
+app.get("/api/get-tables", function(req, res){
+		return res.json(hotTables)
+})
+
+app.get("/api/get-waitlist", function(req, res){
+		return res.json(waitTables)
+});
+
+app.post("/api/make-reservation", function(req, res){
+	var newTable = req.body;
+	newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
+	console.log(newTable);
+
+	res.json(newTable);
+
+	if(hotTables.length<=5){
+		hotTables.push(newTable);
+	}else{
+		waitTables.push(newTable)
+	}
+});
 
 // Listener
 app.listen(PORT, function(){
